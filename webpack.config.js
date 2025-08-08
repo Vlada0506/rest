e95c59eeba09c1ee60e29bf0385d45694e4d53a3
Vlada0506 <vlamaj151208@gmail.com>
2025-08-08 17:57:39 +0500

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-module.exports = {
-    mode: 'development',
-    entry: './src/main1.js',
-    output: {
-        filename: 'main1.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-};
